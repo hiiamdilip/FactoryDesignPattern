4a70dac5cd16141099a2ac29b0f642d5286577c0
hiiamdilip <dilip35kumar@gmail.com>
2020-08-21 12:53:19 +0530

    Update FactoryMain.java

--- FactoryMain.java
+++ FactoryMain.java
@@ -2,8 +2,14 @@ package designPattern;
 
 public class FactoryMain {
     public static void main(String[] args){
-        FactoryPattern o = new FactoryPattern();
-        OS ob = o.getInstance("open");
-        ob.spec();
+        FactoryPattern fP = new FactoryPattern();
+        OS openType = fP.getInstance("open");
+        openType.specification(); //prints I am Powerful
+        
+        OS closeType = fP.getInstance("close");
+        closeType.specification(); //prints I am Secure
+        
+        OS powerType = fP.getInstance("power");
+        powerType.specification(); //prints I am Outdated
     }
 }
